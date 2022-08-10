6a4444197b6246f20655f2eb3fd43008f79486ba
junni1111 <wkszlf98@naver.com>
2022-08-11 04:21:59 +0900

    refactor : swagger topic

--- src/notification/kakao/kakao.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { KAKAO } from '../../util/constants/swagger';
-
-@ApiTags(KAKAO)
-@Controller()
-export class KakaoController {}
--- src/notification/kakao/kakao.module.ts
+++ src/notification/kakao/kakao.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { KakaoController } from './kakao.controller';
 import { KakaoService } from './kakao.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule.register({ timeout: 5000, maxRedirects: 5 })],
-  controllers: [KakaoController],
+  controllers: [],
   providers: [KakaoService],
   exports: [KakaoService],
 })
